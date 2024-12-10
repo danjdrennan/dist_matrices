ff6cb7ee820ce8ffcf521cd4b8f6162a078caf3e
Dan Drennan <danjdrennan@gmail.com>
2024-12-10 16:48:40 -0600

    repo init
    provides a simple reference implementation for people who are in a hurry
    and need a cdist function that is easily extended to other contexts

---
+++ README.md
@@ -0,0 +1,74 @@
+# dist matrices
+
+A short reference implementation of a function for calculating pairwise distance
+functions using only NumPy arrays and broadcasting. The function is most similar
+to `scipy.spatial.distance.cdist` with the signature:
+
+```python
+import numpy as np
+A = np.ndarray
+
+def cdist(x1: A, x2: A) -> A:
+    """Calculate the pairwise distance matrix between two sets of points.
+
+    NOTES:
+    ------
+    - The batch dimensions can be a tuple (), (b,), or (b1, b2, ...).
+
+    - The p dimension must be the same for both inputs and must explicitly exist
+      (i.e., it cannot be implied when p=1).
+
+    ARGS:
+    -----
+    x1: (*batch, n1, p)
+
+    x2: (*batch, n2, p)
+
+    RETURNS:
+    --------
+    distance_matrix (*batch, n1, n2)
+    """
+    ...
+```
+
+Common use cases for the `cdist` function arise in various clustering
+algorithms, Gaussian process regressions, and some graph / network problems.
+This implementation is primarily useful in settings where `cdist` is not already
+available from another framework or library (`scipy`, `torch`, etc.); namely, it
+is not supported in the `jax` ecosystem already.
+
+Two versions of the implementation are provided. One uses a naive implementation
+of Euclidean distance. The other relies on `np.linalg.norm` to calculate norm
+automatically.
+
+## Batch dimensions
+
+The implementation supports broadcasting over batch dimensions explicitly as
+long as `batch` is a tuple of shapes which are consistent between the two
+inputs.
+
+## Changing metrics
+
+With minimal changes, the implementations can be changed to use other distance
+functions. Euclidean distances are most common and simplest, so I've provided
+them.
+
+## Numerical Stability
+
+Numerical stability becomes especially important when using single precision
+floats, hardware accelerators (GPUs), or JIT compiled functions.
+
+The implementations shown here are not optimized for numerical stability. One
+way to improve the stability is to pre-center the inputs before using them.
+Another step is to clamp the min values to a small positive number to avoid
+problems with small negative numbers if one computes square distances directly.
+
+## References
+
+Some helpful references for more background
+
+1. [scipy cdist](https://docs.scipy.org/doc/scipy/reference/generated/scipy.spatial.distance.cdist.html)
+
+2. [torch cdist](https://pytorch.org/docs/stable/generated/torch.cdist.html)
+
+3. [manual implementation](https://discuss.pytorch.org/t/efficient-distance-matrix-computation/9065)
---
+++ dist.py
@@ -0,0 +1,110 @@
+import itertools
+from typing import Callable, NamedTuple
+
+import numpy as np
+
+from numpy.typing import NDArray, DTypeLike
+from scipy.spatial.distance import cdist
+
+A = NDArray[np.float32 | np.float64]
+Fn = Callable[[A, A], A]
+
+
+class Dims(NamedTuple):
+    batch: int
+    n1: int
+    n2: int
+
+
+def diffs(x1: A, x2: A) -> A:
+    return x1[:, None] - x2[None, :]
+
+
+def batched_diffs(x1: A, x2: A) -> A:
+    return x1[..., None, :] - x2[..., None, :, :]
+
+
+def dist(x1: A, x2: A, batch: bool = False) -> A:
+    d = batched_diffs(x1, x2) if batch else diffs(x1, x2)
+    d = np.square(d)
+    sq_diffs: A = np.sum(d, axis=-1)
+    dists = np.sqrt(sq_diffs)
+
+    return dists
+
+
+def dist2(x1: A, x2: A, batch: bool = False) -> A:
+    d = batched_diffs(x1, x2) if batch else diffs(x1, x2)
+    dists: A = np.linalg.norm(d, axis=-1)
+
+    return dists
+
+
+def cmp_dists(
+    x1: A,
+    x2: A,
+    fns: list[Fn],
+    dims: Dims,
+    dtype: DTypeLike,
+    atol: float,
+):
+    def cmp(fn1: Fn, fn2: Fn, dtype: DTypeLike, atol: float):
+        x1_: A = x1.astype(dtype)
+        x2_: A = x2.astype(dtype)
+        b = np.allclose(fn1(x1_, x2_), fn2(x1_, x2_), atol=atol)
+        fail_msg = (
+            f"failed for {fn1.__name__} and {fn2.__name__} at atol {atol} "
+            f"and dtype {dtype} and dims {dims}"
+        )
+        if not b:
+            print(fail_msg)
+
+    for fn1, fn2 in itertools.combinations(fns, 2):
+        cmp(fn1, fn2, dtype, atol)
+
+
+def main():
+    rng = np.random.default_rng(0)
+    atol = 1e-3
+
+    ns = [
+        Dims(batch=1, n1=10, n2=10),
+        Dims(batch=1, n1=50, n2=10),
+        Dims(batch=1, n1=10, n2=50),
+        Dims(batch=10, n1=10, n2=10),
+        Dims(batch=10, n1=50, n2=10),
+        Dims(batch=10, n1=10, n2=50),
+        Dims(batch=100, n1=10, n2=10),
+        Dims(batch=100, n1=50, n2=10),
+        Dims(batch=100, n1=10, n2=50),
+    ]
+    d = [3, 5, 10, 50, 100]
+    dtypes = [np.float32, np.float64]
+
+    # First demo behavior with no batched diffs
+    # Note this part compares to cdist
+    fns: list[Fn] = [
+        cdist,
+        lambda x1, x2: dist(x1, x2, batch=False),
+        lambda x1, x2: dist2(x1, x2, batch=False),
+    ]
+    for n, di, dtype in itertools.product(ns, d, dtypes):
+        x1 = rng.random((n.n1, di)).astype(dtype)
+        x2 = rng.random((n.n2, di)).astype(dtype)
+        cmp_dists(x1, x2, fns, n, dtype, atol)
+
+    # Now demo behavior with batched diffs
+    # Here we skip cdist since it doesn't support batched inputs
+    batched_fns: list[Fn] = [
+        lambda x1, x2: np.stack([cdist(x1[i], x2[i]) for i in range(x1.shape[0])]),
+        lambda x1, x2: dist(x1, x2, batch=True),
+        lambda x1, x2: dist2(x1, x2, batch=True),
+    ]
+    for n, di, dtype in itertools.product(ns, d, dtypes):
+        x1 = rng.random((n.batch, n.n1, di)).astype(dtype)
+        x2 = rng.random((n.batch, n.n2, di)).astype(dtype)
+        cmp_dists(x1, x2, batched_fns, n, dtype, atol)
+
+
+if __name__ == "__main__":
+    main()
